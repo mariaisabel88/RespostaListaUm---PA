c5cb519c77f010a3eb7c8ce01fda1f4deef3322f
Maria Isabel <166646693+mariaisabel88@users.noreply.github.com>
2024-04-10 16:54:50 -0300

    Create Animal.java

---
+++ Animal.java
@@ -0,0 +1,27 @@
+public class Animal {
+	private String nome;
+	private String tipo;
+	private boolean emiteSom;
+	
+	public Animal(String n, String t) {
+		this.nome = n;
+		this.tipo = t;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setNome(String n) {
+		this.nome = n;
+	}
+	public void setTipo(String t) {
+		this.tipo = t;
+	}
+	public String toString() {
+		return "Nome: " + nome + "\nTipo: " + tipo;
+	}
+	public void emitirSom() {
+		this.emiteSom = true;
+	}
