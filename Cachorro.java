fcb57af849cd492a0e762a97cb835a36e8691cdb
Maria Isabel <166646693+mariaisabel88@users.noreply.github.com>
2024-04-11 18:13:52 -0300

    Create Cachorro.java

---
+++ Cachorro.java
@@ -0,0 +1,35 @@
+private String nome;
+		private String raca;
+		private int idade; 
+		
+	public Cachorro(String nome, String raca, int idade) {
+		this.nome = nome;
+		this.raca = raca;
+		this.idade = idade;
+	}
+ 
+ 	public String getNome() { 
+		return nome;
+	}
+ 
+    public void setNome(String nome) {
+    	this.nome = nome;
+    } 	
+   public String getRaca() {
+	   return raca;
+   }
+   
+   public void setRaca(String raca) {
+	   this.raca= raca; 
+   }
+   public int getIdade() {
+	   return idade;
+   }
+    public void setIdade(int idade) {
+    	this.idade = idade; 
+    }
+    public String latir() {
+    	return "Au au!";
+    }
+    	
+   }
